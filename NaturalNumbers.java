2fb82522e4141afeb5a7eac0ebe7c662cb061899
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 10:40:42 +0530

    Create NaturalNumbers.java

---
+++ NaturalNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;  
+      
+    public class SumOfNatualNumbers {  
+      
+        public static void main(String[] args) {  
+      
+            int inputNumber;  
+            int sum;  
+              
+            
+            Scanner scanner = new Scanner(System.in);  
+              
+            
+            System.out.println("Please enter the number :");  
+            inputNumber = scanner.nextInt();  
+              
+            scanner.close();  
+              
+             
+            sum = (inputNumber * (inputNumber+1))/2;  
+              
+             
+            System.out.println("The sum of "+inputNumber+" natural numbers is "+sum);  
+      
+        }  
+      
+    }  
